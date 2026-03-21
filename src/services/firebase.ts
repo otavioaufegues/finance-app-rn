a5db15217fae9227f78ee88b0e20d5b9b74c42e3
Otavio Augusto <otavioaufegues@gmail.com>
2026-03-21 00:49:02 -0300

    credenciais no env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,12 +1,31 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "SUA_KEY",
-  authDomain: "SEU_DOMINIO",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID",
+const getEnv = (key: string) => {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+
+  return value;
 };
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: getEnv("EXPO_PUBLIC_FIREBASE_API_KEY"),
+  authDomain: getEnv("EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN"),
+  projectId: getEnv("EXPO_PUBLIC_FIREBASE_PROJECT_ID"),
+  storageBucket: getEnv("EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: getEnv("EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID"),
+  appId: getEnv("EXPO_PUBLIC_FIREBASE_APP_ID"),
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
